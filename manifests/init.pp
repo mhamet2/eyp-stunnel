b0ff9e9e3b077bb841e96a155ec5604dda706fd7
Marc Herrera <marc.herrera@ntt.eu>
2018-02-01 18:18:41 +0100

    test

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,7 @@ class stunnel(
                             $manage_docker_service = true,
                             $service_ensure        = 'running',
                             $service_enable        = true,
+                            $chroot_path           = '/var/run/stunnel'
                           ) inherits stunnel::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
